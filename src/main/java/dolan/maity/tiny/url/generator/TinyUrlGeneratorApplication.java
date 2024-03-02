276d0eec66af290cc71562023c9ba7f7aad2d49c
maskysans <dolanmaity96@gmail.com>
2024-03-02 20:43:21 +0530

    Project Setup : Initial Commit

---
+++ src/main/java/dolan/maity/tiny/url/generator/TinyUrlGeneratorApplication.java
@@ -0,0 +1,13 @@
+package dolan.maity.tiny.url.generator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TinyUrlGeneratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TinyUrlGeneratorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dolan/maity/tiny/url/generator/TinyUrlGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package dolan.maity.tiny.url.generator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TinyUrlGeneratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
